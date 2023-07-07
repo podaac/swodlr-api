df6665f82daec47a82fdbad925b7c69ee18b7bb4
Joshua Garde <jgarde@jpl.nasa.gov>
2023-07-06 19:18:14 -0700

    Cleanup

--- src/main/java/gov/nasa/podaac/swodlr/granule/Granule.java
+++ src/main/java/gov/nasa/podaac/swodlr/granule/Granule.java
@@ -14,7 +14,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Table(name = "Granules")
