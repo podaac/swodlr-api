39a2fd8d6e6703678027f0bf7b14202bd37f2421
Josh Garde <jgarde@jpl.nasa.gov>
2023-09-13 17:40:57 -0700

    Update product search logic (#12)
    * Update search logic
    
    * Linting
    
    * Fix tests

--- src/main/java/gov/nasa/podaac/swodlr/l2rasterproduct/L2RasterProductRepository.java
+++ src/main/java/gov/nasa/podaac/swodlr/l2rasterproduct/L2RasterProductRepository.java
@@ -6,30 +6,33 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface L2RasterProductRepository extends
     JpaRepository<L2RasterProduct, UUID>, L2RasterProductQuery {
   List<L2RasterProduct> findById(L2RasterProduct product);
 
+  @SuppressWarnings("LineLength")
   @Query(
-      """
-      SELECT p FROM L2RasterProduct p WHERE
-      p.cycle = :cycle and
-      p.pass = :pass and
-      p.scene = :scene and
-      p.outputGranuleExtentFlag = :outputGranuleExtentFlag and
-      p.outputSamplingGridType = :outputSamplingGridType and
-      p.rasterResolution = :rasterResolution and
-      p.utmZoneAdjust = :utmZoneAdjust and
-      p.mgrsBandAdjust = :mgrsBandAdjust
-      """
+      value = """
+      SELECT * FROM \"L2RasterProducts\" WHERE
+      \"cycle\" = :cycle AND
+      \"pass\" = :pass AND
+      \"scene\" = :scene AND
+      \"outputGranuleExtentFlag\" = :outputGranuleExtentFlag AND
+      \"outputSamplingGridType\" = :#{#gridType.toString()} AND
+      \"rasterResolution\" = :rasterResolution AND
+      (\"utmZoneAdjust\" = :utmZoneAdjust OR (:utmZoneAdjust is NULL and \"utmZoneAdjust\" is NULL)) AND
+      (\"mgrsBandAdjust\" = :mgrsBandAdjust OR (:mgrsBandAdjust is NULL and \"mgrsBandAdjust\" is NULL))
+      """,
+      nativeQuery = true
   )
   Optional<L2RasterProduct> findOneByParameters(
       int cycle,
       int pass,
       int scene,
       boolean outputGranuleExtentFlag,
-      GridType outputSamplingGridType,
+      @Param("gridType") GridType outputSamplingGridType,
       int rasterResolution,
       Integer utmZoneAdjust,
       Integer mgrsBandAdjust
