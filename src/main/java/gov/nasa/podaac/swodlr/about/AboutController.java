e1f89f15ec2beeff5d7456f211a9b7423fa8eb35
Joshua Garde <jgarde@jpl.nasa.gov>
2023-11-16 17:53:21 -0800

    Initial about impl

---
+++ src/main/java/gov/nasa/podaac/swodlr/about/AboutController.java
@@ -0,0 +1,28 @@
+package gov.nasa.podaac.swodlr.about;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.util.Properties;
+
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AboutController {
+  private final Properties versionProperties;
+
+  public AboutController() throws IOException {
+    versionProperties = PropertiesLoaderUtils.loadAllProperties("version.properties");
+  }
+
+  @GetMapping("/about")
+  public About getAbout() {
+    return new About(
+      versionProperties.getProperty("version"), 
+      ManagementFactory.getRuntimeMXBean().getUptime(),
+      System.currentTimeMillis()
+    );
+  }
+}
