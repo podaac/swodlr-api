311f681424334297d95279d1c686b51ade5648f1
Joshua Garde <jgarde@jpl.nasa.gov>
2024-06-27 11:51:42 -0700

    Initial implementation of admin endpoints

--- src/main/java/gov/nasa/podaac/swodlr/user/UserController.java
+++ src/main/java/gov/nasa/podaac/swodlr/user/UserController.java
@@ -1,9 +1,13 @@
 package gov.nasa.podaac.swodlr.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.ContextValue;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+
+import gov.nasa.podaac.swodlr.exception.SwodlrException;
 
 @Controller
 public class UserController {
@@ -13,5 +17,17 @@ public class UserController {
   @QueryMapping
   public User currentUser(@ContextValue UserReference userRef) {
     return userRef.fetch();
+  }
+
+  @PreAuthorize("hasRole(\"ROLE_Administrator\")")
+  @QueryMapping
+  public User user(@Argument String username) {
+    var result = userRepository.findByUsername(username);
+    if (result.isEmpty()) {
+      throw new SwodlrException("User not found");
+    }
+
+    User user = result.get();
+    return user;
   }
 }
